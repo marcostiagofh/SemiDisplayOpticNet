c3de9b477e97c117cdc126cf60fa98c7b31b1fa1
Caio Caldeira <caio.caldeira3@gmail.com>
2021-12-28 01:22:51 -0200

    Fix output_path to add switch_size

--- scripts/compress-results.sh
+++ scripts/compress-results.sh
@@ -10,7 +10,7 @@ switch_size=$6
 input_file="input/projectorDS/${dataset}/${num_node}/${sim_id}_tor_${num_node}.txt"
 
 log_path="${project}/projectorDS/${dataset}/"
-filename="${sim_id}_tor_${num_node}"
+filename="${sim_id}_tor_${num_node}_${switch_size}"
 
 zip_path="/media/external-hdd/caioc/${log_path}"
 output_path="output/${log_path}${filename}"
@@ -20,8 +20,6 @@ output_file=${output_path}
 
 sim_stream="output/sims/${sim_id}_${dataset}_${num_node}_${switch_size}.txt"
 cmd="${base_cmd} ${project} -overwrite input=${input_file} switchSize=${switch_size} output=${output_path} AutoStart=true > ${sim_stream}"
-
-set -x
 
 eval $cmd
 
