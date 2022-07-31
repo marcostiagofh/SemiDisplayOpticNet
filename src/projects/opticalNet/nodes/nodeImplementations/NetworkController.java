d3a548a6e7405ecf9ee9c31619f17a6456e78baa
caiocaldeira3 <caio.caldeira3@gmail.com>
2022-07-31 11:39:02 -0300

    Bring the infrastructure of handleMessages and getRotation methods to opticalNet

--- src/projects/opticalNet/nodes/nodeImplementations/NetworkController.java
+++ src/projects/opticalNet/nodes/nodeImplementations/NetworkController.java
@@ -6,10 +6,15 @@ import java.util.PriorityQueue;
 
 import projects.opticalNet.nodes.infrastructureImplementations.LoggerLayer;
 import projects.opticalNet.nodes.messages.HasMessage;
+import projects.opticalNet.nodes.messages.NewMessage;
+import projects.opticalNet.nodes.messages.OpticalNetMessage;
 import projects.opticalNet.nodes.messages.RoutingInfoMessage;
+import projects.opticalNet.nodes.models.Direction;
 import projects.opticalNet.nodes.models.InfraNode;
 import projects.opticalNet.nodes.models.Rotation;
 import sinalgo.gui.transformation.PositionTransformation;
+import sinalgo.nodes.messages.Inbox;
+import sinalgo.nodes.messages.Message;
 import sinalgo.tools.Tools;
 
 /**
@@ -18,6 +23,7 @@ import sinalgo.tools.Tools;
  * outputNodes. It has the rotations implementations as well as their alteration methods.
  */
 public abstract class NetworkController extends LoggerLayer {
+    protected boolean seq = true;
 
     /* Attributes */
     private ArrayList<Boolean> usedNodes;
@@ -676,6 +682,21 @@ public abstract class NetworkController extends LoggerLayer {
     }
 
     /**
+     * Getter for the seq flag.
+     * @return  True if there is a message in the network false if there isn't
+     */
+    public boolean getSeq () {
+        return this.seq;
+    }
+
+    /**
+     * Sets the seq flag as false, called when a message reaches it's destination
+     */
+    public void setSeq () {
+        this.seq = false;
+    }
+
+    /**
      * Returns a boolean true if the cluster type 1 id of both nodes are the same and
      * false if are different
      * @param node1 InfraNode x
@@ -806,8 +827,6 @@ public abstract class NetworkController extends LoggerLayer {
         );
         return apSum + clsId2 - clsId1 - 1;
     }
-
-    protected abstract Rotation getRotationToPerform (InfraNode x, InfraNode dstNode);
 
     /**
      * This method traverses the nodes that still have messages pending routing and locks them
@@ -1025,6 +1044,107 @@ public abstract class NetworkController extends LoggerLayer {
         return false;
     }
 
+        /**
+     * Getter for the rotation a node should perfomed to rout a message to the destination node.
+     * if the message is at one hop away to it's destination or to the LCA between the src node
+     * and the dst node the message is simply routed one time. Else, it returns the appropriated
+     * rotation based on the direction the message needs to be routed and the network topology
+     * surrounding the involved nodes.
+     * @param x         InfraNode with the message
+     * @param dstNode   destination InfraNode
+     * @return          the decided rotation
+     */
+    protected Rotation getRotationToPerform (InfraNode x, InfraNode dstNode) {
+        Direction direction = x.getRoutingDirection(dstNode);
+
+        if (direction == Direction.PARENTROUT) {
+            return Rotation.NULL;
+
+        } else if (direction == Direction.LEFTROUT) {
+            return Rotation.NULL;
+
+        } else if (direction == Direction.RIGHTROUT) {
+            return Rotation.NULL;
+
+        } else if (
+            direction == Direction.PARENT &&
+            !(this.isValidNode(x.getParent()) && this.isValidNode(x.getParent().getParent()))
+        ) {
+            return Rotation.NULL;
+
+        }
+
+        /*bottom-up - BEGIN*/
+        if (direction == Direction.PARENT) {
+            InfraNode y = x.getParent();
+            InfraNode z = y.getParent();
+            if (
+                this.isValidNode(y.getLeftChild()) && x == y.getLeftChild() &&
+                this.isValidNode(z.getLeftChild()) && y == z.getLeftChild()
+            ) {
+                return Rotation.ZIGZIGLEFT_BOTTOMUP;
+
+            } else if (
+                this.isValidNode(y.getRightChild()) && x == y.getRightChild() &&
+                this.isValidNode(z.getRightChild()) && y == z.getRightChild()
+            ) {
+                return Rotation.ZIGZIGRIGHT_BOTTOMUP;
+
+            } else if (
+                this.isValidNode(y.getRightChild()) && x == y.getRightChild() &&
+                this.isValidNode(z.getLeftChild()) && y == z.getLeftChild()
+            ) {
+                return Rotation.ZIGZAGLEFT_BOTTOMUP;
+
+            } else if (
+                this.isValidNode(y.getLeftChild()) && x == y.getLeftChild() &&
+                this.isValidNode(z.getRightChild()) && y == z.getRightChild()
+            ) {
+                return Rotation.ZIGZAGRIGHT_BOTTOMUP;
+
+            } else {
+                Tools.fatalError("Network topology for BottomUp not expected");
+
+            }
+
+        /* Top-Down - LEFT - BEGIN */
+        } else if (direction == Direction.LEFT) {
+            InfraNode y = x.getRoutingNode(dstNode);
+            InfraNode z = y.getRoutingNode(dstNode);
+
+            if (x.getLeftChild() == y && y.getLeftChild() == z) {
+                return Rotation.ZIGZIGLEFT_TOPDOWN;
+
+            } else if (x.getLeftChild() == y && y.getRightChild() == z) {
+                return Rotation.ZIGZAGLEFT_TOPDOWN;
+
+            } else {
+                Tools.fatalError("Network topology for Left TopDown not expected");
+
+            }
+
+        /* Top-Down - RIGHT - BEGIN */
+        } else if (direction == Direction.RIGHT) {
+            InfraNode y = x.getRoutingNode(dstNode);
+            InfraNode z = y.getRoutingNode(dstNode);
+
+            if (x.getRightChild() == y && y.getRightChild() == z) {
+                return Rotation.ZIGZIGRIGHT_TOPDOWN;
+
+            } else if (x.getRightChild() == y && y.getLeftChild() == z) {
+                return Rotation.ZIGZAGRIGHT_TOPDOWN;
+
+            } else {
+                Tools.fatalError("Network topology for Right TopDown not expected");
+
+            }
+        }
+
+        Tools.fatalError("Unexpected rotation");
+
+        return null;
+    }
+
     /**
      * Retrieve the equivalent NetworkNode, inform the next node in the path of the message
      * to the routMsg and send the RoutingInfoMessage to it.
@@ -1039,6 +1159,43 @@ public abstract class NetworkController extends LoggerLayer {
         routMsg.setRoutNodeId(nxtNode.getNetId());
 
         this.sendDirect(routMsg, netNode);
+    }
+
+    /**
+     * This method handles the message a NetworkController receives. If it is a OpticalNetMessage,
+     * it means that this message has reached it's destination, so the number of completed
+     * messages is incremented, the LoggerLayer reports this message information and the weigth
+     * in the path between the src and destination node is updated. If it is a NewMessage the
+     * number of received messages is incremented. If it is a HasMessage, the sender node is
+     * marked as a nodeWithMessage for the controllerStep. If it is a RoutingInfoMessage, the
+     * sender node is marked as a routerNode.
+     */
+    @Override
+    public void handleMessages (Inbox inbox) {
+        while (inbox.hasNext()) {
+            Message msg = inbox.next();
+
+            if (msg instanceof NewMessage) {
+                this.rcvMsgs++;
+
+            } else if (msg instanceof HasMessage) {
+                HasMessage hasmsg = (HasMessage) msg;
+                this.nodesWithMsg.add(hasmsg);
+
+            } else if (msg instanceof RoutingInfoMessage) {
+                RoutingInfoMessage routmsg = (RoutingInfoMessage) msg;
+                this.routingNodes.add(routmsg);
+
+            } else if (msg instanceof OpticalNetMessage) {
+                OpticalNetMessage optmsg = (OpticalNetMessage) msg;
+                this.logIncrementCompletedRequests();
+                this.logMessageRouting(optmsg.getRouting());
+
+                this.cmpMsgs++;
+                this.seq = true;
+            }
+
+        }
     }
 
     /**
